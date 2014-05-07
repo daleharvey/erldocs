2a48f69d06c05c07f7666551cedb7db0466561ea
Pierre Fenoll <pierrefenoll@gmail.com>
2014-05-07 13:40:49 +0200

    A script to build erldocs.com docs.

---
+++ otp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Generate erldocs docs from Erlang/OTP's tarball
+
+[[ $# -eq 0 ]] && echo "Usage: $0  ‹path to untar'ed OTP›⁺" && exit 1
+
+idir="${1%%/}" # Remove trailing slash if exists
+release="${idir##otp_src_}"
+odir="docs-$release"
+erldocs='./erldocs'
+
+mkdir -p  "$odir"
+rm    -rf "$odir"/*
+[[ ! -x "$erldocs" ]] && [[ ! -L $"$erldocs" ]] && \
+    echo "$erldocs executable not found!" && exit 1
+
+includes="-I $idir"/erts/include
+for dir in "$idir"/lib/*/include "$idir"/bootstrap/lib/*/include; do
+    includes="$includes -I $dir"
+done
+
+"$erldocs"          \
+    -o "$odir"      \
+    $includes       \
+    "$idir"/lib/*   \
+    "$idir"/erts    \
+    | tee _"$release"
+
+rm -rf "$odir"/.xml
+tar jcf "$idir".tar.bz2 "$idir"
+
+shift && [[ "$1" = '' ]] && exit 0 || $0 $*
