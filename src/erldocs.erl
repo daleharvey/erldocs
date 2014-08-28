d45b80c40e8ad6a38708d294f753de6e8fbc8b85
Pierre Fenoll <pierrefenoll@gmail.com>
2014-08-28 19:59:33 +0200

    That would never match, erlc did not complain #fear.

--- src/erldocs.erl
+++ src/erldocs.erl
@@ -45,15 +45,15 @@ parse (["-o", Dest | Rest], Conf) ->
 parse (["-I", Include | Rest], #conf{includes = Includes} = Conf) ->
     parse(Rest, Conf#conf{includes = [absp(Include)|Includes]});
 
+parse (["--ga", GA | Rest], Conf) ->
+    parse(Rest, Conf#conf{ga = GA});
+
 parse ([Dir0 | Rest], #conf{dirs = Dirs} = Conf) ->
     case Dir0 of
         "." -> Dir = cwd();
         _   -> Dir = Dir0
     end,
-    parse(Rest, Conf#conf{dirs = [absp(Dir)|Dirs]});
-
-parse (["--ga", GA | Rest], Conf) ->
-    parse(Rest, Conf#conf{ga = GA}).
+    parse(Rest, Conf#conf{dirs = [absp(Dir)|Dirs]}).
 
 
 run (Conf) ->
