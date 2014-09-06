fc41169fe61abdf367dfa4fd299c5a095a552ac3
Pierre Fenoll <pierrefenoll@gmail.com>
2014-09-06 03:09:59 +0200

    maint.sh : less frequent git pushes.

--- maint.sh
+++ maint.sh
@@ -61,6 +61,8 @@ mv -v  "$odir" "$site"
 mv -v  "$odir".tar.bz2 "$archive"
 mv -v  _"$release" "$site"/log-"$release".txt
 
+modifs=$(cd "$site_root" && git status --porcelain | wc -l)
+[[ "$modifs" -eq 2 ]] && echo "No interesting changes to push." && exit 0
 cd "$site_root" \
     && git add "$release" \
     && git add  archives/ \
